9125f9bbddbb8500adeafbe07da53acd600223ef
Muhammad Imran <hame353678@gmail.com>
2026-09-10 21:55:39 -0700

    app/Assignments/page.tsx

--- app/Assignments/page.tsx
+++ app/Assignments/page.tsx
@@ -12,7 +12,7 @@ export default async function Assignements() {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <div className="text-xl font-bold text-center text-gray-800 mb-4">
+      <div className="text-xl font-bold text-center bg-cyan-100 text-gray-800 mb-4">
         Assigments
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
