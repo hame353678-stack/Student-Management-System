8f365017a6dd4b371fe44cb2bf8ca8e186ea2ddc
Muhammad Imran <hame353678@gmail.com>
2026-09-11 08:51:02 -0700

    all

--- ServerActions/login.ts
+++ ServerActions/login.ts
@@ -6,18 +6,20 @@ import { redirect } from "next/navigation";
 
 export default async function Login(formdata: FormData) {
   const username = formdata.get("username") as string;
-  const regid = formdata.get("regid") as string;
+  const regId = formdata.get("regid") as string;
   const client = await clientPromise;
   const database = client.db("StudentManagement");
   const management = database.collection<studentType>("Students");
   const student: studentType | null = await management.findOne({
     name: username,
-    regId: regid,
+    regId: regId,
   });
-  const id = student?.id;
+
   if (!student) {
+    console.log(`Not found student with Id : {id}`);
     redirect(`/Account/SignUp`);
   } else {
+    const id = student.id;
     redirect(`/Students/${id}`);
   }
 }
