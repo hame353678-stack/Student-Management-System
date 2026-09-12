d39de039b6dd5b0e7f6b41674f7d7fefdda89476
Muhammad Imran <hame353678@gmail.com>
2026-09-12 01:48:00 -0700

    all

--- ServerActions/DeleteStudent.ts
+++ ServerActions/DeleteStudent.ts
@@ -5,15 +5,15 @@ import clientPromise from "@/lib/db";
 import { redirect } from "next/navigation";
 
 export default async function DeleteStudent(formdata: FormData) {
-  const regId = formdata.get("regId");
-  const name = formdata.get("name");
+  const regId = formdata.get("regId") as string;
+  const name = formdata.get("name") as string;
   const client = await clientPromise;
   const database = client.db("StudentManagement");
   const management = database.collection<studentType>("Students");
-  const student = await management.findOne({ regId: regId, name: name });
+  const student = await management.findOne({ name: name, regId: regId });
   if (student) {
-    const deleted = management.deleteOne(student);
+    const deleted = await management.deleteOne(student);
     console.log(deleted);
-    redirect("/Students");
+    redirect("/Admin?secret=Pet123");
   }
 }
