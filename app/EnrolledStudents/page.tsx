8e4926540e3c3b24c53ac46dbab9ccb6d2132a4e
Muhammad Imran <hame353678@gmail.com>
2026-09-12 09:52:01 -0700

    all

--- app/EnrolledStudents/page.tsx
+++ app/EnrolledStudents/page.tsx
@@ -10,6 +10,9 @@ export default async function Students() {
 
     return (
       <div className="grid grid-cols-3 bg-fuchsia-300 sm:gap-1 md:pag-2 lg:gap-3 sm:2 md:3 lg:p-4 ">
+        <h1 className="text-center col-span-3 text-2xl font-bold">
+          Enrolled Students
+        </h1>
         {students.map((student, idx) => (
           <ShowStudents student={student} key={idx} />
         ))}
