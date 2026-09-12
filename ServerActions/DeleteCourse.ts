19e1b0430a6220a05edbf35fea78af5df609666b
Muhammad Imran <hame353678@gmail.com>
2026-09-11 20:32:27 -0700

    all

--- ServerActions/DeleteCourse.ts
+++ ServerActions/DeleteCourse.ts
@@ -4,20 +4,12 @@ import { courseType } from "@/DataTypes/courseType";
 import clientPromise from "@/lib/db";
 import { redirect } from "next/navigation";
 
-export default async function DELETE({
-  searchParams,
-}: {
-  searchParams: Promise<{
-    id: string;
-    courseName: string;
-    duration: string;
-  }>;
-}) {
-  const { id, courseName, duration } = await searchParams;
+export default async function DELETE(formdata: FormData) {
+  const id = formdata.get("id") as string;
   const client = await clientPromise;
   const database = client.db("StudentManagement");
   const management = database.collection<courseType>("Courses");
-  const course = await management.findOne({ id: id });
+  const course = await management.findOne({ id: parseInt(id) });
   if (course) {
     const deleted = management.deleteOne(course);
     console.log(deleted);
