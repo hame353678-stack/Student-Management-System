e0a6ed7144a896d89e43d73983fa6d51592c8ca7
Muhammad Imran <hame353678@gmail.com>
2026-09-12 02:53:32 -0700

    all

---
+++ ServerActions/DeleteQuize.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { QuizeType } from "@/DataTypes/QuizesType";
+import clientPromise from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export default async function DeleteQuize(formdata: FormData) {
+  const coursname = formdata.get("coursename") as string;
+  const QuizeTitle = formdata.get("QuizeTitle") as string;
+  const InstructorName = formdata.get("InstructorName") as string;
+
+  const client = await clientPromise;
+  const database = client.db("StudentManagement");
+  const management = database.collection<QuizeType>("Quizes");
+  const Quize = await management.findOne({
+    coursename: coursname,
+    QuizeTitle: QuizeTitle,
+    InstructorName: InstructorName,
+  });
+  if (Quize) {
+    management.deleteOne(Quize);
+    revalidatePath("/AdminQuizes");
+    redirect("/AdminQuizes");
+  } else {
+    redirect("/Admin?secret=Pet123");
+  }
+}
