fc73b9518c0762214e87862dac75371379f9ce71
Muhammad Imran <hame353678@gmail.com>
2026-09-11 20:36:58 -0700

    all

---
+++ ServerActions/AdminAddAssignment.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { AssignmentTypes } from "@/DataTypes/AssignmentType";
+import clientPromise from "@/lib/db";
+export default async function AdminAddAssignment(formdata: FormData) {
+  const assignmentTitle = formdata.get("assignmentTitle") as string;
+  const courseName = formdata.get("courseName") as string;
+  const instructorName = formdata.get("instructorName") as string;
+  const submissionDate = formdata.get("submissionDate") as string;
+
+  const client = await clientPromise;
+  const database = client.db("StudentManagement");
+  const Assignments = database.collection<AssignmentTypes>("Assignments");
+
+  const data: AssignmentTypes = {
+    assignmentTitle: assignmentTitle,
+    courseName: courseName,
+    instructorName: instructorName,
+    submissionDate: submissionDate,
+  };
+
+  if (Assignments) {
+    const Assignment = await Assignments.insertOne(data);
+    console.log(Assignment);
+  }
+}
