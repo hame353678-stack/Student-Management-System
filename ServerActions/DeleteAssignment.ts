cdc949d38ecb4de5ee82f0fa477452def0eb97b8
Muhammad Imran <hame353678@gmail.com>
2026-09-11 22:56:38 -0700

    all

--- ServerActions/DeleteAssignment.ts
+++ ServerActions/DeleteAssignment.ts
@@ -1,18 +1,21 @@
 "use server";
-import clientPromise from "@/lib/db";
+
 import { AssignmentTypes } from "@/DataTypes/AssignmentType";
+import clientPromise from "@/lib/db";
 import { redirect } from "next/navigation";
-export default async function DELETE(formdata: FormData) {
-  const courseName = formdata.get("courseName");
-  const assignmentTitle = formdata.get("assignmentTitle");
+export default async function DeleteAssignment(formdata: FormData) {
+  const assignmentTitle = formdata.get("assignmentTitle") as string;
+  const courseName = formdata.get("courseName") as string;
+
   const client = await clientPromise;
   const database = client.db("StudentManagement");
   const management = database.collection<AssignmentTypes>("Assignments");
-  const Assignment = await management.findOne({name:courseName,Title:assignmentTitle});
+  const Assignment = await management.findOne({
+    courseName: courseName,
+    assignmentTitle: assignmentTitle,
+  });
   if (Assignment) {
-    const deleted = await management.deleteOne(Assignment);
-    console.log(deleted);
-    redirect("/AdminAssignments");
+    await management.deleteOne(Assignment);
+    redirect("/Admin?secret=Pet123");
   }
 }
-
