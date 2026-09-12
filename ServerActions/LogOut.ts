08b57322ad180a06142f5ddbeba2767c5748692f
Muhammad Imran <hame353678@gmail.com>
2026-09-12 02:25:19 -0700

    all

--- ServerActions/LogOut.ts
+++ ServerActions/LogOut.ts
@@ -2,6 +2,7 @@
 
 import { studentType } from "@/DataTypes/studentType";
 import clientPromise from "@/lib/db";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export default async function LogOut(formdata: FormData) {
@@ -15,6 +16,7 @@ export default async function LogOut(formdata: FormData) {
   if (student) {
     const deleted = await management.deleteOne(student);
     console.log(deleted);
-    redirect("/Admin?secret=Pet123");
+    revalidatePath("/");
+    redirect("/");
   }
 }
