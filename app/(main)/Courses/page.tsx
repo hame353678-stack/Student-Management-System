1a6c360ba2ea14bad8b2a3b4b98dcd35f817dc99
Muhammad Imran <hame353678@gmail.com>
2026-09-12 19:26:17 -0700

    all

--- app/(main)/Courses/page.tsx
+++ app/(main)/Courses/page.tsx
@@ -1,15 +1,21 @@
-import clientPromise from "@/lib/db";
+import { useEffect, useState } from "react";
 import ShowCourses from "@/Components/ShowCourses";
 import { courseType } from "@/DataTypes/courseType";
 
 export default async function Courses() {
-  try {
-    const client = await clientPromise;
-    const database = client.db("StudentManagement");
-    const management = database.collection<courseType>("Courses");
+  const [courses, setcourses] = useState<courseType[]>([]);
 
-    const courses: courseType[] | null = await management.find({}).toArray();
+  useEffect(() => {
+    const fetchcourses = async () => {
+      const data = await fetch("/api/courses");
+      const fetchedcourses = await data.json();
 
+      setcourses(fetchedcourses);
+    };
+
+    fetchcourses();
+  }, []);
+  if (courses) {
     return (
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 bg-gray-100 gap-5 p-5">
         {courses.map((course: courseType, idx: number) => (
@@ -17,9 +23,7 @@ export default async function Courses() {
         ))}
       </div>
     );
-  } catch (err) {
-    console.log(err);
-
+  } else {
     return (
       <div className="p-5 text-center font-semibold text-gray-600">
         Not found courses
