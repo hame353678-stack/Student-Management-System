2556d274428ea12c96ba17de48243d4b3fb846e7
Muhammad Imran <hame353678@gmail.com>
2026-09-12 18:57:44 -0700

    all

--- app/EnrolledStudents/page.tsx
+++ app/EnrolledStudents/page.tsx
@@ -1,28 +1,32 @@
-import ShowStudents from "@/Components/ShowStudent";
-import clientPromise from "@/lib/db";
-import { studentType } from "@/DataTypes/studentType";
-import { enrollementType } from "@/DataTypes/enrolementType";
-export default async function Students() {
-  try {
-    const client = await clientPromise;
-    const database = client.db("StudentManagement");
-    const management = database.collection<enrollementType>("Enrolements");
-    const students: enrollementType[] | null = await management
-      .find({})
-      .toArray();
+"use client";
 
-    return (
-      <div className="grid grid-cols-3 bg-fuchsia-300 sm:gap-1 md:pag-2 lg:gap-3 sm:2 md:3 lg:p-4 ">
-        <h1 className="text-center col-span-3 text-2xl font-bold">
-          Enrolled Students
-        </h1>
-        {students.map((student, idx) => (
-          <ShowStudents student={student} key={idx} />
-        ))}
-      </div>
-    );
-  } catch (err) {
-    console.log(err);
-    return <div>Not found students</div>;
-  }
+import ShowStudents from "@/Components/ShowStudent";
+import { enrollementType } from "@/DataTypes/enrolementType";
+import { useEffect, useState } from "react";
+
+export default function Students() {
+  const [students, setstudents] = useState<enrollementType[]>([]);
+
+  useEffect(() => {
+    const fetchstudents = async () => {
+      const data = await fetch("/api/enrolledStudents");
+      const fetcheddata = await data.json();
+
+      setstudents(fetcheddata);
+    };
+
+    fetchstudents();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-3 bg-fuchsia-300 sm:gap-1 md:gap-2 lg:gap-3 sm:p-2 md:p-3 lg:p-4">
+      <h1 className="text-center col-span-3 text-2xl font-bold">
+        Enrolled Students
+      </h1>
+
+      {students.map((student, idx) => (
+        <ShowStudents student={student} key={idx} />
+      ))}
+    </div>
+  );
 }
