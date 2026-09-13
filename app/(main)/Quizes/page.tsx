b9a21d550803b46d4cf95db95ffc7cbcfdaf0904
Muhammad Imran <hame353678@gmail.com>
2026-09-12 19:28:55 -0700

    all

--- app/(main)/Quizes/page.tsx
+++ app/(main)/Quizes/page.tsx
@@ -1,20 +1,28 @@
-import clientPromise from "@/lib/db";
+import { useEffect, useState } from "react";
 import { QuizeType } from "@/DataTypes/QuizesType";
 import ShowQuizes from "@/Components/ShowQuizes";
 
 export default async function Quizes() {
-  const client = await clientPromise;
-  const database = client.db("StudentManagement");
-  const management = database.collection<QuizeType>("Quizes");
-  const Quizes: QuizeType[] | null = await management.find({}).toArray();
-  if (Quizes) {
+  const [Quizzes, setQuizzes] = useState<QuizeType[]>([]);
+
+  useEffect(() => {
+    const fetchQuizzes = async () => {
+      const data = await fetch("/api/courses");
+      const fetchedQuizzes = await data.json();
+
+      setQuizzes(fetchedQuizzes);
+    };
+
+    fetchQuizzes();
+  }, []);
+  if (Quizzes) {
     return (
       <div className="p-4 max-w-7xl mx-auto">
         <div className="text-xl font-bold text-center bg-cyan-100 text-gray-800 mb-4">
           Quizes
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {Quizes.map((Quize, idx) => (
+          {Quizzes.map((Quize, idx) => (
             <ShowQuizes Quize={Quize} key={idx} />
           ))}
         </div>
