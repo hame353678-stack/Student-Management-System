902b55bd195d423267021d21e633940040ce656b
Muhammad Imran <hame353678@gmail.com>
2026-09-12 20:11:29 -0700

    all

--- app/(main)/Assignments/page.tsx
+++ app/(main)/Assignments/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState, useEffect } from "react";
 import { AssignmentTypes } from "@/DataTypes/AssignmentType";
 import ShowAssignments from "@/Components/ShowAssignments";
