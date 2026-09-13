7ea4fd640d4b8e2c251b948d5b8976093dd8244b
Muhammad Imran <hame353678@gmail.com>
2026-09-12 19:18:46 -0700

    all

--- app/AdminCourses/page.tsx
+++ app/AdminCourses/page.tsx
@@ -1,17 +1,27 @@
-import clientPromise from "@/lib/db";
+"use client";
 import AdminShowCourses from "@/Components/AdminShowCourses";
 import { courseType } from "@/DataTypes/courseType";
-
-export default async function Courses() {
+import Link from "next/link";
+import { useEffect, useState } from "react";
+export default function Courses() {
+  const [courses, setcourses] = useState<courseType[]>([]);
   try {
-    const client = await clientPromise;
-    const database = client.db("StudentManagement");
-    const management = database.collection<courseType>("Courses");
-
-    const courses: courseType[] | null = await management.find({}).toArray();
-
+    useEffect(() => {
+      const fetchcourses = async () => {
+        const data = await fetch("/api/courses");
+        const fetchedcourses = await data.json();
+        setcourses(fetchedcourses);
+      };
+      fetchcourses();
+    }, []);
     return (
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 bg-gray-100 gap-5 p-5">
+        <Link
+          href="/AddnewCourse"
+          className="flex-1 text-center bg-blue-500 text-white py-2 px-3 rounded-lg font-semibold text-sm hover:bg-blue-600"
+        >
+          Add New Course
+        </Link>
         {courses.map((course: courseType, idx: number) => (
           <AdminShowCourses course={course} key={idx} />
         ))}
