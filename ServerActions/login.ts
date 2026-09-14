87c520495fdae899474f074adedad3461e614cd6
Muhammad Imran <hame353678@gmail.com>
2026-09-13 18:42:09 -0700

    all

--- ServerActions/login.ts
+++ ServerActions/login.ts
@@ -2,7 +2,6 @@
 
 import { studentType } from "@/DataTypes/studentType";
 import clientPromise from "@/lib/db";
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
@@ -30,7 +29,6 @@ export default async function Login(formdata: FormData) {
       path: "/",
       maxAge: 60 * 60 * 24 * 7, // 1 week
     });
-    revalidatePath("/Home");
     redirect("/Home");
   }
 }
